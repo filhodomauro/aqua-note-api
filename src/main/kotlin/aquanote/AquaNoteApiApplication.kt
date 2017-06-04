29f58394666574f02b20e166fbf3b0c0098047aa
Mauro Filho <mauro.filho@gmail.com>
2017-06-03 21:03:14 -0300

    refactor packages

--- src/main/kotlin/aquanote/AquaNoteApiApplication.kt
+++ src/main/kotlin/aquanote/AquaNoteApiApplication.kt
@@ -1,4 +1,4 @@
-package com.filhodomauro.aquanote
+package aquanote
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
--- src/test/kotlin/aquanote/AquaNoteApiApplicationTests.kt
+++ src/test/kotlin/aquanote/AquaNoteApiApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.filhodomauro.aquanote
+package aquanote
 
 import org.junit.Test
 import org.junit.runner.RunWith
